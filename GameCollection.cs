6b997c3e551fb6640b983da89370928ba1d35212
nick <68024347+nkbooth@users.noreply.github.com>
2022-12-02 23:57:31 -0500

    * Refactor for second star

--- GameCollection.cs
+++ GameCollection.cs
@@ -12,9 +12,33 @@ namespace AdventOfCode_02
                 string[] gameData = line.Split(" ");
                 RockPaperScissorsGame game = new RockPaperScissorsGame();
                 game.TheirMove = gameData[0];
-                game.MyMove = gameData[1];
+                game.MyMove = CalculateMove(gameData);
                 Games.Add(game);
             }
+        }
+
+        public string CalculateMove(string[] data)
+        {
+            string strategy = data[1];
+            string theirLastMove = data[0];
+            
+            if (strategy.ToUpper() == "X")
+            {
+                if (theirLastMove == "A") { return "C";}
+                if(theirLastMove == "B") { return "A"; }
+                if(theirLastMove == "C") { return "B"; }
+            }
+            if (strategy.ToUpper() == "Y")
+            {
+                return theirLastMove;
+            }
+            if (strategy.ToUpper() == "Z")
+            {
+                if(theirLastMove == "A") { return "B"; }
+                if(theirLastMove == "B") { return "C"; }
+                if(theirLastMove == "C") { return "A"; }
+            }
+            return "error";
         }
     }
 }
